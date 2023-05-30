c9466fd3f4809e5fa74c8f12e2bd56426ea70650
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-05-29 23:36:49 -0400

    Created starting point for Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './styles/App.css';
+import './styles/App.css'
+import Home from './components/routes/Home'
 
 const App = () => {
   return (
     <div className="App">
       <h1>Project manager</h1>
+      <Home />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/routes/Home.tsx
@@ -0,0 +1,11 @@
+import '../../styles/Home.css'
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h2>Home</h2>
+    </div>
+  )
+}
+
+export default Home
