25801a99cd71d73938f478f92eab18968d1e0598
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-05-29 23:40:08 -0400

    Created NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './styles/App.css'
+import NavBar from './components/NavBar'
 import Home from './components/routes/Home'
 
 const App = () => {
   return (
     <div className="App">
+      <NavBar />
       <h1>Project manager</h1>
       <Home />
     </div>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import '../styles/NavBar.css'
+
+const NavBar = () => {
+  return (
+    <nav className='NavBar'>
+      <h2>Navbar</h2>
+    </nav>
+  )
+}
+
+export default NavBar
