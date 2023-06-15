776422adb8ae5e3169cfe73795858d6a570a29a8
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-15 00:02:00 -0400

    Added login form

--- src/components/routes/Login.tsx
+++ src/components/routes/Login.tsx
@@ -4,6 +4,14 @@ const Login = () => {
   return (
     <div className='Login'>
       <h1>Login</h1>
+
+      <form>
+        <label htmlFor='email'>Email</label>
+        <input type='email' id='email' name='email' required />
+
+        <label htmlFor='password'>Password</label>
+        <input type='password' id='password' name='password' required />
+      </form>
     </div>
   )
 }
--- src/components/routes/Register.tsx
+++ src/components/routes/Register.tsx
@@ -20,7 +20,6 @@ const Register = () => {
         
         <label htmlFor='username'>Username</label>
         <input type='text' id='username' name='username' required />
-
       </form>
     </div>
   )
