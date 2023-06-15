cc640e61f683b397ce10f298353d45879a5cfa1e
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-14 23:53:30 -0400

    Added form and blank inputs

--- src/components/routes/Register.tsx
+++ src/components/routes/Register.tsx
@@ -4,6 +4,12 @@ const Register = () => {
   return (
     <div className='Register'>
       <h1>Register</h1>
+
+      <form>
+        <input type='text' />
+        <input type='text' />
+        <input type='text' />
+      </form>
     </div>
   )
 }
