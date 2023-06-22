8340916c1e5d6239bd7f5a27677f2701e861ccc1
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-21 23:57:05 -0400

    Imported todos and bugs

--- src/components/routes/Project.tsx
+++ src/components/routes/Project.tsx
@@ -1,9 +1,14 @@
 import '../../styles/Project.css'
+import Todos from '../Todos'
+import Bugs from '../Bugs'
 
 const Project = () => {
   return (
     <div className='Project'>
       <h1>Project</h1>
+
+      <Todos />
+      <Bugs />
     </div>
   )
 }
