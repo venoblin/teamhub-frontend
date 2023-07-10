5eae5bdbc34e3c6c100d4742f115e5f8db5981d3
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-09 23:32:22 -0400

    Created component

---
+++ src/components/RecentProjects.tsx
@@ -0,0 +1,9 @@
+const RecentProjects = () => {
+  return (
+    <div className="RecentProjects">
+      <h2>Recent Projects</h2>
+    </div>
+  )
+}
+
+export default RecentProjects
--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -1,9 +1,13 @@
 import '../../styles/Home.css'
+import RecentProjects from '../RecentProjects'
+import Feed from '../Feed'
 
 const Home = () => {
   return (
     <div className="Home">
       <h1>Home</h1>
+      <RecentProjects />
+      <Feed />
     </div>
   )
 }
