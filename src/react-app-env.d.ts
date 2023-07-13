8ea4ba008f82ffdd658b42b6f4a18f254f849738
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-12 23:57:35 -0400

    created type

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,13 @@
 /// <reference types="react-scripts" />
+
+import { ReactNode } from "react"
+
 type Payload = {
   email: string,
   password: string
+}
+
+type UiProps = {
+  children: ReactNode,
+  className?: string = ''
 }
