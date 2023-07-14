365b3fc4d374894cae56f39226472dcd9326beaf
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-13 21:46:14 -0400

    updated class

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Project from './components/routes/Project'
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
