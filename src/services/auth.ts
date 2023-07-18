85cd64978ae6b8c807c8490455834e89e7541b68
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-17 23:26:23 -0400

    modified routes

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,7 +3,7 @@ import Client from "./api"
 
 export const SignInUser = async (data: Payload) => {
   try {
-    const res = await Client.post('/auth/login', data)
+    const res = await Client.post('/login', data)
     localStorage.setItem('token', res.data.token)
     return res.data.user
   } catch (error) {
@@ -13,7 +13,7 @@ export const SignInUser = async (data: Payload) => {
 
 export const RegisterUser = async (data: Payload) => {
   try {
-    const res = await Client.post('/auth/register', data)
+    const res = await Client.post('/register', data)
     return res.data
   } catch (error) {
     throw error
@@ -22,7 +22,7 @@ export const RegisterUser = async (data: Payload) => {
 
 export const CheckSession = async () => {
   try {
-    const res = await Client.get('/auth/session')
+    const res = await Client.get('/session')
     return res.data
   } catch (error) {
     throw error
