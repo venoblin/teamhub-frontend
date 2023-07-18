3bf872f10f80f6b181bd3e65cdfec38ab23d4ab4
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-17 23:48:14 -0400

    worked on hook

---
+++ src/hooks/useFormState.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const useFormState = (...args: string[]) => {
+  const initObj: any = {}
+  args.forEach((arg: string) => {
+    initObj[arg] = ''
+  })
+  const [state, setState] = useState({initObj})
+
+  return [state, setState]
+}
+
+export default useFormState
