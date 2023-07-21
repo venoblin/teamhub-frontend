50eeb135c6c7c0a0ae1145ee872b539f4b4ab25b
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-20 23:57:45 -0400

    modified structure of inputs

--- src/components/routes/Register.tsx
+++ src/components/routes/Register.tsx
@@ -1,4 +1,5 @@
 import '../../styles/Register.css'
+import InputHandler from '../../classes/InputHandler'
 import useFormState from '../../hooks/useFormState'
 
 const Register = () => {
@@ -15,19 +16,43 @@ const Register = () => {
 
       <form>
         <label htmlFor='email'>Email</label>
-        <input type='email' id='email' name='email' required />
+        <input type='email'
+          id='email'
+          name='email'
+          required 
+        />
 
         <label htmlFor='password'>Password</label>
-        <input type='password' id='password' name='password' required />
+        <input 
+          type='password' 
+          id='password' 
+          name='password' 
+          required 
+        />
 
         <label htmlFor='confirmPassword'>Confirm Password</label>
-        <input type='password' id='confirmPassword' name='confirmPassword' required />
+        <input 
+          type='password' 
+          id='confirmPassword' 
+          name='confirmPassword' 
+          required 
+        />
         
         <label htmlFor="name">Name</label>
-        <input type='text' id='name' name='name' required />
+        <input 
+          type='text' 
+          id='name' 
+          name='name' 
+          required 
+        />
         
         <label htmlFor='username'>Username</label>
-        <input type='text' id='username' name='username' required />
+        <input 
+          type='text' 
+          id='username' 
+          name='username' 
+          required 
+        />
       </form>
     </div>
   )
