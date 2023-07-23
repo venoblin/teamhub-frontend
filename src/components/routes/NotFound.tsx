85a923ec54f2a5f1258b28b5499d32cd50780d8c
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-23 17:08:11 -0400

    created not found route component

---
+++ src/components/routes/NotFound.tsx
@@ -0,0 +1,11 @@
+import '../../styles/NotFound.css'
+
+const NotFound = () => {
+  return (
+    <div className='not-found'>
+      <h1>404 Not Found!</h1>
+    </div>
+  )
+}
+
+export default NotFound
