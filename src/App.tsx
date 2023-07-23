796b107eca092821bbfd23ce2721d84fdabbdae8
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-23 17:22:03 -0400

    added redirection if use isnt authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ const App = () => {
           <Route path='*' element={<NotFound />} />
         </Routes>
       )}
-            
     </div>
   )
 }
--- src/components/routes/NotFound.tsx
+++ src/components/routes/NotFound.tsx
@@ -1,6 +1,18 @@
 import '../../styles/NotFound.css'
+import { useEffect, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { UserContext } from '../../contexts/UserContext'
 
 const NotFound = () => {
+  const userContext = useContext(UserContext)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!userContext?.authenticated) {
+      navigate('/login')
+    }
+  }, [])
+  
   return (
     <div className='not-found'>
       <h1>404 Not Found!</h1>
