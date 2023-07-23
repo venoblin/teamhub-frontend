f59a94b65573d431f2dceed4990e1b746cc0ea5a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-23 12:52:26 -0400

    finished hook

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,5 +1,13 @@
 import { useState } from "react"
 
-const useToggle = (initialState = false) => {}
+const useToggle = (initialState: Boolean = false) => {
+  const [toggleState, setToggleState] = useState(initialState)
+
+  const toggle = () => {
+    setToggleState(!toggleState)
+  }
+
+  return [toggleState, toggle]
+}
 
 export default useToggle
