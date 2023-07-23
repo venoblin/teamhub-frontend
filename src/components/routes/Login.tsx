283e9d85542f0bb0d1ef5455da6f757c9c55424d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-23 17:39:37 -0400

    added login functionality

--- src/components/routes/Login.tsx
+++ src/components/routes/Login.tsx
@@ -1,15 +1,26 @@
 import '../../styles/Login.css'
-import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { LoginUser } from '../../services/auth'
+import { UserContext } from '../../contexts/UserContext'
 import FormHandler from '../../classes/FormHandler'
 import InputHandler from '../../classes/InputHandler'
 import useFormState from '../../hooks/useFormState'
 
 const Login = () => {
+  const userContext = useContext(UserContext)
   const [formState, setFormState, resetFormState] = useFormState('email', 'password')
+  const navigate = useNavigate()
 
   const loginUser = async () => {
-    console.log('hi')
+    try {
+      const payload = await LoginUser(formState)
+      userContext?.setUser(payload)
+      userContext?.toggleAuthenticated(true)
+      navigate('/')
+    } catch (err) {
+      console.log('There was an error!')
+    }
     resetFormState()
   }
 
