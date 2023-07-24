06416d94905a0e3acc466e16f90ae68b541ba29f
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-23 20:21:33 -0400

    added logout functionality and conditional rendering for log out link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,25 @@
 import '../styles/NavBar.css'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { UserContext } from '../contexts/UserContext'
 
 const NavBar = () => {
+  const userContext = useContext(UserContext)
+
+  const logoutHandler = () => {
+    userContext?.handleLogout()
+  }
+  
   return (
     <nav className='navbar'>
       <h2>Project Manager</h2>
 
       <div className='links'>
-        <Link to={'/'}>Home</Link>
+        <Link to='/'>Home</Link>
+
+        {userContext?.authenticated && 
+          <Link to='/login' onClick={logoutHandler}>Log Out</Link>
+        }
       </div>
     </nav>
   )
