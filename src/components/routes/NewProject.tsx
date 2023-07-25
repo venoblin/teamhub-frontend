7065942af2f44b4b8fd4c3139a7c852315fd03ac
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-24 22:27:03 -0400

    created inputs and form

--- src/components/routes/NewProject.tsx
+++ src/components/routes/NewProject.tsx
@@ -4,6 +4,26 @@ const NewProject = () => {
   return (
     <div className='new-poject'>
       <h1>New Project</h1>
+
+      <form>
+        <label htmlFor="name">Name</label>
+        <input 
+          type='name' 
+          id='name' 
+          name='name' 
+          required 
+        />
+
+        <label htmlFor="gitUrl">Git URL</label>
+        <input 
+          type='gitUrl' 
+          id='gitUrl' 
+          name='gitUrl' 
+          required 
+        />
+
+        <button>Create</button>
+      </form>
     </div>
   )
 }
