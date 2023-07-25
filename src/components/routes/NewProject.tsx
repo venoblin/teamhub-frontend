8fd19f2fe4dce9b56455f4fe748317e9c5fa7642
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-24 21:37:43 -0400

    created new project component

---
+++ src/components/routes/NewProject.tsx
@@ -0,0 +1,11 @@
+import '../../styles/NewProject.css'
+
+const NewProject = () => {
+  return (
+    <div className='new-poject'>
+      <h1>New Project</h1>
+    </div>
+  )
+}
+
+export default NewProject
