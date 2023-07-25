8a7df18fd8c6cae8a7db0b7803289748c4433691
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-24 21:42:39 -0400

    added on click function

--- src/components/RecentProjects.tsx
+++ src/components/RecentProjects.tsx
@@ -1,11 +1,18 @@
 import '../styles/RecentProjects.css'
+import { useNavigate } from 'react-router-dom'
 import Panel from './ui/Panel'
 
 const RecentProjects = () => {
+  const navigate = useNavigate()
+  
+  const newHandler = () => {
+    navigate('/projects/new')
+  }
+  
   return (
     <Panel className="recent-projects">
       <h2>Recent Projects</h2>
-      <button>New</button>
+      <button onClick={newHandler}>New</button>
     </Panel>
   )
 }
