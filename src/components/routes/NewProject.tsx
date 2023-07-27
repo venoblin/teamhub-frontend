d40fbb09013ae9d4ee2466bf9bb0bd22df003e18
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-26 23:54:34 -0400

    made giturl optional

--- src/components/routes/NewProject.tsx
+++ src/components/routes/NewProject.tsx
@@ -14,6 +14,7 @@ const NewProject = () => {
   ])
 
   const createProject = () => {
+    // CreateProject({})
     resetFormState()
   }
   
--- src/types/project.d.ts
+++ src/types/project.d.ts
@@ -1,5 +1,5 @@
 export interface Project {
   name: String,
-  gitUrl: String,
+  gitUrl?: String,
   ownerId: Number
 }
