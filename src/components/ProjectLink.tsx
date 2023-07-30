9cb3965e9c298c5b0cdbfdc25557213e88c389ee
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-07-30 13:38:49 -0400

    added styles

--- src/components/ProjectLink.tsx
+++ src/components/ProjectLink.tsx
@@ -5,7 +5,7 @@ import { ProjectLinkProps } from '../types/props'
 const ProjectLink = (props: ProjectLinkProps) => {
   
   return (
-    <Link to={`/projects/${props.project.id}`}>
+    <Link to={`/projects/${props.project.id}`} className='project-link'>
       {`${props.owner.username}/${props.project.name}`}
     </Link>
   )
