db29c59ae949df61f164d4969a6856508e39f970
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-04 16:18:08 -0400

    modified types

--- src/types/props.d.ts
+++ src/types/props.d.ts
@@ -2,11 +2,10 @@ export interface ProjectPropsType extends Props {
   project: ProjectType
 }
 
-export interface UiPropsType extends React.PropsWithChildren {
-  className?: string
+export interface ProjectLinkPropsType extends ProjectPropsType {
+  owner: UserType
 }
 
-export interface ProjectLinkPropsType {
-  project: ProjectType,
-  owner: UserType
+export interface UiPropsType extends React.PropsWithChildren {
+  className?: string
 }
