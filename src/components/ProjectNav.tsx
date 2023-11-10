cd46f2738d79b6d91ba293fb13755c6b3ef8cb16
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-09 21:00:00 -0500

    added conditional rendering for git link

--- src/components/ProjectNav.tsx
+++ src/components/ProjectNav.tsx
@@ -6,11 +6,15 @@ const ProjectNav = (props: ProjectLinkPropsType) => {
   return (
     <nav>
       <div className='links'>
-        <Link to={`/${props.owner.username}/${props.project?.name}`} target='_blank'>Feed</Link>
+        <Link to={`/${props.owner.username}/${props.project?.name}`}>Feed</Link>
       </div>
 
       <div className='icon-links'>
-        <Link to='' target='_blank'>Git</Link>
+        {props.project?.git_url.length ? (
+          <Link to='' target='_blank'>Git</Link>
+        ) : (
+          ''
+        )}
       </div>
     </nav>
   )
