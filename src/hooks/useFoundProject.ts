14b01c6c0e4be6883c654f3b1539bf31c3c14a45
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-14 23:42:14 -0500

    finished hook

--- src/hooks/useFoundProject.ts
+++ src/hooks/useFoundProject.ts
@@ -1,9 +1,22 @@
-import { useState, useContext } from 'react'
+import { useContext } from 'react'
 import { UserContext } from '../contexts/UserContext'
 import useProject from './useProject'
+import { ProjectType } from '../types/project'
 
-const useFoundProject = () => {
+const useFoundProject = (projectName: string | undefined): [ProjectType, React.Dispatch<React.SetStateAction<ProjectType>>] => {
+  const userContext = useContext(UserContext)
+  const [project, setProject] = useProject()
 
+  if (typeof projectName !== 'undefined') {
+    const foundProject = userContext?.findProject(projectName)
+
+    if (foundProject) {
+      setProject(foundProject)
+      return [project, setProject]
+    }
+  }
+
+  return [project, setProject]
 }
 
 export default useFoundProject
