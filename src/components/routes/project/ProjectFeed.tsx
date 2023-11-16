acdf9d77f29b9af71901717a3a4bf64abacb5671
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-15 19:36:34 -0500

    added bug and todos

--- src/components/routes/project/ProjectFeed.tsx
+++ src/components/routes/project/ProjectFeed.tsx
@@ -1,10 +1,15 @@
 import '../../../styles/ProjectFeed.css'
 import { ProjectPropsType } from '../../../types/props'
+import Todos from '../../Todos'
+import Bugs from '../../Bugs'
 
-const ProjectFeed = (project: ProjectPropsType) => {
+const ProjectFeed = (props: ProjectPropsType) => {
   return (
     <div className='project-feed'>
-      <h1>Project feed</h1>
+      <h1>Feed</h1>
+
+      <Todos project={props.project} />
+      <Bugs project={props.project} />
     </div>
   )
 }
