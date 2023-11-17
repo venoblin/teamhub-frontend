4108cf373810244b0734747c629a86dec6a0e5f7
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-16 19:25:15 -0500

    created new component

---
+++ src/components/routes/project/ProjectTodos.tsx
@@ -0,0 +1,11 @@
+import '../../../styles/ProjectTodos.css'
+
+const ProjectTodos = () => {
+  return (
+    <div className='project-todos'>
+      <h1>Project todos</h1>
+    </div>
+  )
+}
+
+export default ProjectTodos
