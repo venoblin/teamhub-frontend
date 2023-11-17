71d5f89f320fe2f5325ba06fb29a2e08add3f29e
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-16 20:21:34 -0500

    removed imports

--- src/components/routes/project/ProjectFeed.tsx
+++ src/components/routes/project/ProjectFeed.tsx
@@ -1,7 +1,5 @@
 import '../../../styles/ProjectFeed.css'
 import { ProjectPropsType } from '../../../types/props'
-import Todos from '../../Todos'
-import Bugs from '../../Bugs'
 
 const ProjectFeed = (props: ProjectPropsType) => {
   return (
