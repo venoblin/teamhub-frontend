95235f6fc246c36f539d92c7738f99f2208ba392
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-16 21:36:06 -0500

    added form with add toggle

--- src/components/routes/project/ProjectBugs.tsx
+++ src/components/routes/project/ProjectBugs.tsx
@@ -1,11 +1,54 @@
 import '../../../styles/ProjectBugs.css'
+import useToggle from '../../../hooks/useToggle'
+import useFormState from '../../../hooks/useFormState'
 import { ProjectPropsType } from '../../../types/props'
+import InputHandler from '../../../classes/InputHandler'
+import FormHandler from '../../../classes/FormHandler'
 import Bugs from '../../Bugs'
 
 const ProjectBugs = (props: ProjectPropsType) => {
+  const [addMode, toggleAddMode] = useToggle(false)
+  const [formState, setFormState, resetFormState] = useFormState(['bug', 'bug_info'])
+
+  const createBug = () => {
+    resetFormState()
+  }
+
+  const toggleMode = () => {
+    toggleAddMode()
+  }
+  
   return (
     <div className='project-bugs'>
       <h1>Bugs</h1>
+
+      <button onClick={toggleMode}>
+        {addMode ? 'X' : 'Add'}
+      </button>
+
+      {addMode && 
+        <form onSubmit={(evt) => FormHandler.submit(evt, createBug)}>
+          <label htmlFor="bug">Bug</label>
+          <input 
+            type='bug' 
+            id='bug' 
+            name='bug' 
+            required
+            value={formState.bug}
+            onChange={(evt) => InputHandler.changeListen(evt, formState, setFormState)}
+          />
+
+          <label htmlFor="bug_info">Bug Info (optional)</label>
+          <input 
+            type='bug_info' 
+            id='bug_info' 
+            name='bug_info'
+            value={formState.bug_info}
+            onChange={(evt) => InputHandler.changeListen(evt, formState, setFormState)}
+          />
+          <button>Create</button>
+        </form>
+      }
 
       <Bugs project={props.project} />
     </div>
