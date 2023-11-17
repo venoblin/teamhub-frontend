bedecde2ee88fc49bb29a4e7d06c244b3206e52f
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-16 19:26:58 -0500

    created new component

---
+++ src/components/routes/project/ProjectBugs.tsx
@@ -0,0 +1,11 @@
+import '../../../styles/ProjectBugs.css'
+
+const ProjectBugs = () => {
+  return (
+    <div className='project-bugs'>
+      <h1>Project todos</h1>
+    </div>
+  )
+}
+
+export default ProjectBugs
