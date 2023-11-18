a7bdd7158d3e1581a55af859d70309d2d1cd43bf
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-18 18:53:28 -0500

    changed to localhost

--- src/global/index.ts
+++ src/global/index.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL: string = process.env.REACT_APP_DB_URL || 'http://127.0.0.1:5000'
+export const API_BASE_URL: string = process.env.REACT_APP_DB_URL || 'http://localhost:5000'
