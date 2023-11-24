802ddde59e0e59baf7509752a80c2282b6b82967
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-24 10:08:14 -0500

    started work on deleting todo

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,10 +1,21 @@
 import '../styles/TodoCard.css'
+import { useContext } from 'react'
+import { UserContext } from '../contexts/UserContext'
 import { TodoPropsType } from '../types/props'
 import Card from './ui/Card'
 
 const TodoCard = (props: TodoPropsType) => {
+  const userContext = useContext(UserContext)
+
+  const deleteHandler = async () => {
+  }
+  
   return (
-    <Card>{props.singleTodo.todo}</Card>
+    <Card>
+      <p>{props.singleTodo.todo}</p>
+      
+      <button>DELETE</button>
+    </Card>
   )
 }
 
