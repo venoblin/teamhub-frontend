1f343e9399a3f051cfc8401ce01d434d5b53e820
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-01-09 23:38:22 -0500

    modified interfaces

--- src/types/bug.d.ts
+++ src/types/bug.d.ts
@@ -1,10 +1,17 @@
 export interface BugPayloadType {
-  bug?: string,
-  bug_info?: string,
-  project_id?: number
+  bug: string,
+  bug_info: string,
+  project_id: number
 }
 
 export interface BugType extends BugPayloadType {
   id: number,
   completed: boolean
+}
+
+export interface BugPatchType {
+  bug?: string,
+  bug_info?: string,
+  project_id?: number,
+  completed?: boolean
 }
--- src/types/todo.d.ts
+++ src/types/todo.d.ts
@@ -1,9 +1,15 @@
 export interface TodoPayloadType {
-  todo?: string,
-  project_id?: number
+  todo: string,
+  project_id: number
 }
 
 export interface TodoType extends TodoPayloadType {
   id: number,
   completed: boolean
+}
+
+export interface TodoPatchType {
+  todo?: string,
+  project_id?: number
+  completed?: boolean
 }
