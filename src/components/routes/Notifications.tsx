7ba353472b2261583200aa0ae0cc98f7e20b08e9
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-01-15 12:47:31 -0500

    created notification route files

---
+++ src/components/routes/Notifications.tsx
@@ -0,0 +1,11 @@
+import '../../styles/Notifications.css'
+
+const Notifications = () => {
+  return (
+    <div className='notifications'>
+      <h2>Notifications</h2>
+    </div>
+  )
+}
+
+export default Notifications
