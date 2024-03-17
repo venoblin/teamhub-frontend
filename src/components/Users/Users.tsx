48a936bb856a94654899da4f3fef71c6cf257bf9
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-03-17 13:32:35 -0400

    added error popup

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,17 +6,21 @@ import { submit } from '../../utils/formHandler'
 import { GetUserByIdentifier } from '../../services'
 import useUser from '../../hooks/useUser'
 import UserCard from '../UserCard/UserCard'
+import useToggle from '../../hooks/useToggle'
+import ErrorPopup from '../ErrorPopup/ErrorPopup'
 
 const Users = () => {
   const [formState, setFormState, resetFormState] = useFormState(['identifier'])
   const [user, setUser] = useUser()
+  const [isError, toggleIsError] = useToggle()
 
   const submitHandler = async () => {
     try {
       const user = await GetUserByIdentifier(formState.identifier)
       setUser({...user})
     } catch (err: any) {
-      console.log(err.response.message)
+      console.log(err.response.data.message)
+      toggleIsError()
     }
 
     resetFormState()
@@ -43,6 +47,10 @@ const Users = () => {
 
       {user.id && (
         <UserCard user={user} />
+      )}
+
+      {isError && (
+        <ErrorPopup message="ERROR" onClose={toggleIsError} />
       )}
     </Panel>
   )
