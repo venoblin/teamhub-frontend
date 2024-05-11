ef0947f447e125ec1ed0eba56d8eb46daa2513f3
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-05-10 23:09:10 -0400

    added try and catch to all methods

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -32,7 +32,6 @@ export const UserProvider = (props: PropsWithChildren) => {
   const getAndSetUser = async (id: number) => {
     try {
       const userRes = await GetUser(id)
-
       setUser({
         id: userRes.id,
         username: userRes.username,
@@ -67,159 +66,190 @@ export const UserProvider = (props: PropsWithChildren) => {
   }
 
   const checkToken = async () => {
-    const userPayload: UserType = await CheckSession()
-    
-    if (userPayload.id) await getAndSetUser(userPayload.id)
+    try {
+      const userPayload: UserType = await CheckSession()
+      if (userPayload.id) await getAndSetUser(userPayload.id)
+    } catch {
+      throw new Error('Error checking session!')
+    }  
   }
 
   const findProject = (name: string) => {
     let foundProject = null
-
     userProjects.forEach(project => {
       if (project.name === name) foundProject = project
     })
-
     return foundProject
   }
 
   const postProject = async (payload: ProjectPayloadType) => {
-    if (user.id) {
+    try {
       const newProject = await PostProject({
         name: payload.name,
         git_url: payload.git_url,
         owner_id: user.id
       })
-
       const updatedProjects = [...userProjects]
       updatedProjects.push(newProject)
       setUserProjects(updatedProjects)
+    } catch {
+      throw new Error('Error posting project!')
     }
   }
 
   const deleteProject = async (project: ProjectType) => {
-    await DeleteProject(project.id)
-
-    const updatedProjects = [...userProjects]
-    updatedProjects.forEach((p, i) => {
-      if (p.id === project.id) {
-        updatedProjects.splice(i, 1);
-      }
-    })
-    setUserProjects(updatedProjects)
+    try {
+      await DeleteProject(project.id)
+      const updatedProjects = [...userProjects]
+      updatedProjects.forEach((p, i) => {
+        if (p.id === project.id) {
+          updatedProjects.splice(i, 1);
+        }
+      })
+      setUserProjects(updatedProjects)
+    } catch {
+      throw new Error('Error in deleting project!')
+    }    
   }
 
   const patchProject = async (project: ProjectType, update: ProjectPatchType, setProject: React.Dispatch<React.SetStateAction<ProjectType>>) => {
-    await PatchProject(project.id, update)
-    
-    const newProject = {...project, ...update}
-    const updatedProjects = updateObjInArr(userProjects, project, newProject)
-    
-    setProject(newProject)
-    setUserProjects(updatedProjects)  
+    try {
+      await PatchProject(project.id, update)
+      const newProject = {...project, ...update}
+      const updatedProjects = updateObjInArr(userProjects, project, newProject)  
+      setProject(newProject)
+      setUserProjects(updatedProjects)  
+    } catch {
+      throw new Error('Error in patching project!')
+    }
   }
 
   const postTodo = async (payload: TodoPayloadType, project: ProjectType, setProject: React.Dispatch<React.SetStateAction<ProjectType>>) => {
-    const todo = await PostTodo(payload)
-
-    const newProject = {...project, todos: [...project.todos, todo]}
-
-    const updatedProjects = updateObjInArr(userProjects, project, newProject)
-
-    setProject(newProject)
-    setUserProjects(updatedProjects)
+    try {
+      const todo = await PostTodo(payload)
+      const newProject = {...project, todos: [...project.todos, todo]}
+      const updatedProjects = updateObjInArr(userProjects, project, newProject)
+      setProject(newProject)
+      setUserProjects(updatedProjects)
+    } catch {
+      throw new Error('Error in posting todo!')
+    }
   }
 
   const deleteTodo = async (project: ProjectType, todoId: number | null, setProject: React.Dispatch<React.SetStateAction<ProjectType>>) => {
-    await DeleteTodo(todoId)
-
-    const newTodos = project.todos.filter(t => t.id !== todoId)
-    const newProject = {...project, todos: newTodos}
-    const updatedProjects = updateObjInArr(userProjects, project, newProject)
-    setProject(newProject)
-    setUserProjects(updatedProjects)
+    try {
+      await DeleteTodo(todoId)
+      const newTodos = project.todos.filter(t => t.id !== todoId)
+      const newProject = {...project, todos: newTodos}
+      const updatedProjects = updateObjInArr(userProjects, project, newProject)
+      setProject(newProject)
+      setUserProjects(updatedProjects)  
+    } catch {
+      throw new Error('Error in deleting todo!')
+    }
   }
 
   const postBug = async (payload: BugPayloadType, project: ProjectType, setProject: React.Dispatch<React.SetStateAction<ProjectType>>) => {
-    const bug = await PostBug(payload)
-
-    const newProject = {...project, bugs: [...project.bugs, bug]}
-
-    const updatedProjects = updateObjInArr(userProjects, project, newProject)
-    setProject(newProject)
-    setUserProjects(updatedProjects)
+    try {
+      const bug = await PostBug(payload)
+      const newProject = {...project, bugs: [...project.bugs, bug]}
+      const updatedProjects = updateObjInArr(userProjects, project, newProject)
+      setProject(newProject)
+      setUserProjects(updatedProjects)
+    } catch {
+      throw new Error('Error in posting bug')
+    }
   }
 
   const deleteBug = async (project: ProjectType, bugId: number | null, setProject: React.Dispatch<React.SetStateAction<ProjectType>>) => {
-    await DeleteBug(bugId)
-
-    const newBugs = project.bugs.filter(b => b.id !== bugId)
-    const newProject = {...project, bugs: newBugs}
-    const updatedProjects = updateObjInArr(userProjects, project, newProject)
-    setProject(newProject)
-    setUserProjects(updatedProjects)
+    try {
+      await DeleteBug(bugId)
+      const newBugs = project.bugs.filter(b => b.id !== bugId)
+      const newProject = {...project, bugs: newBugs}
+      const updatedProjects = updateObjInArr(userProjects, project, newProject)
+      setProject(newProject)
+      setUserProjects(updatedProjects)  
+    } catch {
+      throw new Error('Error in deleting bug!')
+    }
   }
 
   const patchBug = async (project: ProjectType, bug: BugType, update: BugPatchType, setProject: React.Dispatch<React.SetStateAction<ProjectType>>) => {
-    await PatchBug(bug.id, update)
-
-    let newEvent = null
-    if (update.completed) {
-      newEvent = await PostEvent({
-        event: `Completed "${bug.bug}" bug`,
-        project_id: project.id
-      })
+    try {
+      await PatchBug(bug.id, update)
+      let newEvent = null
+      if (update.completed) {
+        newEvent = await PostEvent({
+          event: `Completed "${bug.bug}" bug`,
+          project_id: project.id
+        })
+      }
+      const newEvents = newEvent !== null ? [...project.events, newEvent] : [...project.events]
+      const newBugs = project.bugs.map(b => b.id === bug.id ? {...b, ...update} : b)
+      const newProject = {...project, bugs: newBugs, events: newEvents}
+      const updatedProjects = updateObjInArr(userProjects, project, newProject)
+      setProject(newProject)
+      setUserProjects(updatedProjects)
+    } catch {
+      throw new Error('Error in patching bug!')
     }
-
-    const newEvents = newEvent !== null ? [...project.events, newEvent] : [...project.events]
-    const newBugs = project.bugs.map(b => b.id === bug.id ? {...b, ...update} : b)
-    const newProject = {...project, bugs: newBugs, events: newEvents}
-    const updatedProjects = updateObjInArr(userProjects, project, newProject)
-    setProject(newProject)
-    setUserProjects(updatedProjects)
   }
 
   const patchTodo = async (project: ProjectType, todo: TodoType, update: TodoPatchType, setProject: React.Dispatch<React.SetStateAction<ProjectType>>) => {
-    await PatchTodo(todo.id, update)
+    try {
+      await PatchTodo(todo.id, update)
 
-    let newEvent = null
-    if (update.completed) {
-      newEvent = await PostEvent({
-        event: `Completed "${todo.todo}" todo`,
-        project_id: project.id
-      })
+      let newEvent = null
+      if (update.completed) {
+        newEvent = await PostEvent({
+          event: `Completed "${todo.todo}" todo`,
+          project_id: project.id
+        })
+      }
+      
+      const newEvents = newEvent !== null ? [...project.events, newEvent] : [...project.events]
+      const newTodos = project.todos.map(t => t.id === todo.id ? {...t, ...update} : t)
+      const newProject = {...project, todos: newTodos, events: newEvents}
+      const updatedProjects = updateObjInArr(userProjects, project, newProject)
+      setProject(newProject)
+      setUserProjects(updatedProjects)
+    } catch {
+      throw new Error('Error in patching todo!')
     }
-    
-    const newEvents = newEvent !== null ? [...project.events, newEvent] : [...project.events]
-    const newTodos = project.todos.map(t => t.id === todo.id ? {...t, ...update} : t)
-    const newProject = {...project, todos: newTodos, events: newEvents}
-    const updatedProjects = updateObjInArr(userProjects, project, newProject)
-    setProject(newProject)
-    setUserProjects(updatedProjects)
   }
 
   const postNotification = async (payload: NotificationPayloadType) => {
-    const newNotification = await PostNotification(payload)
-    setUserNotifications([...userNotifications, newNotification])
+    try {
+      const newNotification = await PostNotification(payload)
+      setUserNotifications([...userNotifications, newNotification])
+    } catch {
+      throw new Error('Error in posting notification!')
+    }
   }
 
   const patchNotification = async (notification: NotificationType, payload: NotificationPatchType) => {
-    const update = await PatchNotification(notification.id, payload)
-
-    const updatedNotifications = updateObjInArr(userNotifications, notification, update)
-
-    setUserNotifications([...updatedNotifications])
+    try {
+      const update = await PatchNotification(notification.id, payload)
+      const updatedNotifications = updateObjInArr(userNotifications, notification, update)
+      setUserNotifications([...updatedNotifications])
+    } catch {
+      throw new Error('Error in patching notification!')
+    }
   }
 
   const deleteNotification = async (notification: NotificationType) => {
-    await DeleteNotification(notification.id)
-
-    const updatedNotifications = [...userNotifications]
-    updatedNotifications.forEach((n, i) => {
-      if (n.id === notification.id) {
-        updatedNotifications.splice(i, 1);
-      }
-    })
-    setUserNotifications(updatedNotifications)
+    try {
+      await DeleteNotification(notification.id)
+      const updatedNotifications = [...userNotifications]
+      updatedNotifications.forEach((n, i) => {
+        if (n.id === notification.id) {
+          updatedNotifications.splice(i, 1);
+        }
+      })
+      setUserNotifications(updatedNotifications)
+    } catch {
+      throw new Error('Error in deleting notification!')
+    }
   }
 
   useEffect(() => {
